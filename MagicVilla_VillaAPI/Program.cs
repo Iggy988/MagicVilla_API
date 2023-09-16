2045f14b24f3d3332ec70f9d49f130a81d26d8c3
Iggy988 <igor.rodic1988@gmail.com>
2023-09-16 10:15:33 +0200

    Adding Http return for NotAcceptable and add XmlDataContractsSerializerFormaters

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -2,7 +2,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddControllers(option =>
+{
+    //ako format nije prihvacen -> vratice poruku za Header-Accept: application/xml (npr)
+    option.ReturnHttpNotAcceptable = true;
+})
+    .AddNewtonsoftJson()
+    .AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

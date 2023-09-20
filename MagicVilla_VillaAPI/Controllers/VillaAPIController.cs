b6218171e7a8c4cc775a5c41a87e0f5f977620ce
Iggy988 <igor.rodic1988@gmail.com>
2023-09-20 23:04:14 +0200

    Implementing AutoMapper in API Controller

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,7 +1,9 @@
-﻿using MagicVilla_VillaAPI.Data;
+﻿using AutoMapper;
+using MagicVilla_VillaAPI.Data;
 using MagicVilla_VillaAPI.Logging;
 using MagicVilla_VillaAPI.Models;
 using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -15,15 +17,17 @@ public class VillaAPIController : ControllerBase
 {
     private readonly ILogger<VillaAPIController> _logger;
     private readonly ApplicationDbContext _db;
+    private readonly IMapper _mapper;
 
     //private readonly ILogging _logger;
 
 
-    public VillaAPIController(ILogger<VillaAPIController> logger /*ILogging logger*/, ApplicationDbContext db)
+    public VillaAPIController(ILogger<VillaAPIController> logger /*ILogging logger*/, ApplicationDbContext db, IMapper mapper)
     {
         //_logger = logger;
         _logger = logger;
         _db = db;
+        _mapper = mapper;
     }
 
 
@@ -31,7 +35,10 @@ public class VillaAPIController : ControllerBase
     public async Task<ActionResult<IEnumerable<VillaDTO>>> GetVillas()
     {
         _logger.LogInformation("Get All Villas", "");
-        return Ok(await _db.Villas.ToListAsync());
+
+        IEnumerable<Villa> villaList = await _db.Villas.ToListAsync();
+        //to convert villaList to VillaDTO using automapper
+        return Ok(_mapper.Map<List<VillaDTO>>(villaList));
     }
 
     [HttpGet("{id:int}", Name = "GetVilla")]
@@ -54,46 +61,49 @@ public class VillaAPIController : ControllerBase
         {
             return NotFound();
         }
-        return Ok(villa);
+        return Ok(_mapper.Map<VillaDTO>(villa));
     }
 
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<ActionResult<VillaDTO>> CreateVilla([FromBody]VillaCreateDTO villaDTO) 
+    public async Task<ActionResult<VillaDTO>> CreateVilla([FromBody]VillaCreateDTO createDTO) 
     {
         //if (!ModelState.IsValid)
         //{
         //    return BadRequest(ModelState);
         //}
         //custom validation
-        if (await _db.Villas.FirstOrDefaultAsync(u => u.Name.ToLower() == villaDTO.Name.ToLower()) != null)
+        if (await _db.Villas.FirstOrDefaultAsync(u => u.Name.ToLower() == createDTO.Name.ToLower()) != null)
         {
             //key moze biti prazan
             ModelState.AddModelError("CustomError", "Villa already Exists!");
             return BadRequest(ModelState);
         }
 
-        if (villaDTO == null)
+        if (createDTO == null)
         {
-            return BadRequest(villaDTO);
+            return BadRequest(createDTO);
         }
         //if (villaDTO.Id > 0)
         //{
         //    return StatusCode(StatusCodes.Status500InternalServerError);
         //}
         //vilaDTO.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id +1;
-        Villa model = new()
-        {
-            Amenity = villaDTO.Amenity,
-            Details = villaDTO.Details,
-            ImageUrl = villaDTO.ImageUrl,
-            Name = villaDTO.Name,
-            Occupancy = villaDTO.Occupancy,
-            Rate = villaDTO.Rate,
-            Sqft = villaDTO.Sqft
-        };
+        
+        Villa model = _mapper.Map<Villa>(createDTO);
+        //ne trebamo da mappiramo samostalno model to dto, zato sto koristimo automapper
+        //Villa model = new()
+        //{
+        //    Amenity = createDTO.Amenity,
+        //    Details = createDTO.Details,
+        //    ImageUrl = createDTO.ImageUrl,
+        //    Name = createDTO.Name,
+        //    Occupancy = createDTO.Occupancy,
+        //    Rate = createDTO.Rate,
+        //    Sqft = createDTO.Sqft
+        //};
         
         await _db.Villas.AddAsync(model);
         await _db.SaveChangesAsync();
@@ -128,9 +138,9 @@ public class VillaAPIController : ControllerBase
     [HttpPut("{id:int}", Name = "UpdateVilla")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<IActionResult> UpdateVilla(int id, [FromBody]VillaDTO villaDTO)
+    public async Task<IActionResult> UpdateVilla(int id, [FromBody]VillaDTO updateDTO)
     {
-        if (villaDTO == null || id != villaDTO.Id)
+        if (updateDTO == null || id != updateDTO.Id)
         {
             return BadRequest();
         }
@@ -139,18 +149,20 @@ public class VillaAPIController : ControllerBase
         //villa.Name = villaDTO.Name;
         //villa.Sqft = villaDTO.Sqft;
         //villa.Occupancy = villaDTO.Occupancy;
-
-        Villa model = new()
-        {
-            Amenity = villaDTO.Amenity,
-            Details = villaDTO.Details,
-            Id = villaDTO.Id,
-            ImageUrl = villaDTO.ImageUrl,
-            Name = villaDTO.Name,
-            Occupancy = villaDTO.Occupancy,
-            Rate = villaDTO.Rate,
-            Sqft = villaDTO.Sqft
-        };
+        
+        //ne trebamo da mappiramo samostalno model to dto, zato sto koristimo automapper
+        Villa model = _mapper.Map<Villa>(updateDTO);
+        //Villa model = new()
+        //{
+        //    Amenity = updateDTO.Amenity,
+        //    Details = updateDTO.Details,
+        //    Id = updateDTO.Id,
+        //    ImageUrl = updateDTO.ImageUrl,
+        //    Name = updateDTO.Name,
+        //    Occupancy = updateDTO.Occupancy,
+        //    Rate = updateDTO.Rate,
+        //    Sqft = updateDTO.Sqft
+        //};
         _db.Villas.Update(model);
         await _db.SaveChangesAsync();
 
@@ -170,18 +182,19 @@ public class VillaAPIController : ControllerBase
         // moramo dodati AsNoTracking() jer ce EF track dva Id (VillaDTO i Villa) a to ne moze
         var  villa = await _db.Villas.AsNoTracking().FirstOrDefaultAsync(u => u.Id == id);
 
-
-        VillaUpdateDTO villaDTO = new()
-        {
-            Amenity = villa.Amenity,
-            Details = villa.Details,
-            Id = villa.Id,
-            ImageUrl = villa.ImageUrl,
-            Name = villa.Name,
-            Occupancy = villa.Occupancy,
-            Rate = villa.Rate,
-            Sqft = villa.Sqft
-        };
+        VillaUpdateDTO villaDTO = _mapper.Map<VillaUpdateDTO>(villa);
+        //ne trebamo da mappiramo samostalno model to dto, zato sto koristimo automapper
+        //VillaUpdateDTO villaDTO = new()
+        //{
+        //    Amenity = villa.Amenity,
+        //    Details = villa.Details,
+        //    Id = villa.Id,
+        //    ImageUrl = villa.ImageUrl,
+        //    Name = villa.Name,
+        //    Occupancy = villa.Occupancy,
+        //    Rate = villa.Rate,
+        //    Sqft = villa.Sqft
+        //};
 
         if (villa == null)
         {
@@ -190,17 +203,18 @@ public class VillaAPIController : ControllerBase
 
         patchDTO.ApplyTo(villaDTO, ModelState);
 
-        Villa model = new()
-        {
-            Amenity = villaDTO.Amenity,
-            Details = villaDTO.Details,
-            Id = villaDTO.Id,
-            ImageUrl = villaDTO.ImageUrl,
-            Name = villaDTO.Name,
-            Occupancy = villaDTO.Occupancy,
-            Rate = villaDTO.Rate,
-            Sqft = villaDTO.Sqft
-        };
+        Villa model = _mapper.Map<Villa>(villaDTO);
+        //Villa model = new()
+        //{
+        //    Amenity = villaDTO.Amenity,
+        //    Details = villaDTO.Details,
+        //    Id = villaDTO.Id,
+        //    ImageUrl = villaDTO.ImageUrl,
+        //    Name = villaDTO.Name,
+        //    Occupancy = villaDTO.Occupancy,
+        //    Rate = villaDTO.Rate,
+        //    Sqft = villaDTO.Sqft
+        //};
 
         _db.Villas.Update(model);
         await _db.SaveChangesAsync();

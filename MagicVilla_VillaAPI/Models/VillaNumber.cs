684c430247eb57b9ccbf9238546e4dfccbbd875d
Iggy988 <igor.rodic1988@gmail.com>
2023-09-25 22:16:33 +0200

    Implementing APIResponse in Controller

---
+++ MagicVilla_VillaAPI/Models/VillaNumber.cs
@@ -0,0 +1,9 @@
+﻿namespace MagicVilla_VillaAPI.Models;
+
+public class VillaNumber
+{
+    public int VillaNo { get; set; }
+    public string SpecialDetails { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime UpdatedDate { get; set; }
+}

1a46b356239d967e6baa1fd8dc116fa5b031ecd3
Iggy988 <igor.rodic1988@gmail.com>
2023-09-27 22:37:44 +0200

    Add AutoMapper in web project

---
+++ MagicVilla_WebProject/MappingConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using MagicVilla_WebProject.Models.Dto;
+
+namespace MagicVilla_WebProject;
+
+public class MappingConfig : Profile
+{
+    public MappingConfig()
+    {
+        //mapping source to destination
+        CreateMap<VillaDTO, VillaCreateDTO>().ReverseMap();
+        CreateMap<VillaDTO, VillaUpdateDTO>().ReverseMap();
+
+        CreateMap<VillaNumberDTO, VillaNumberCreateDTO>().ReverseMap();
+        CreateMap<VillaNumberDTO, VillaNumberUpdateDTO>().ReverseMap();
+        
+      
+    }
+}
+
+

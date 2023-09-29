f4e8e84e8eef8d8bf71fa96e323033e8ad64690e
Iggy988 <igor.rodic1988@gmail.com>
2023-09-29 23:29:53 +0200

    Adding BaseService

---
+++ MagicVilla_WebProject/Services/IServices/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using MagicVilla_VillaAPI.Models;
+using MagicVilla_WebProject.Models;
+
+namespace MagicVilla_WebProject.Services.IServices;
+
+public interface IBaseService
+{
+    APIResponse responseModel { get; set; }
+    Task<T> SendAsync<T>(APIRequest apiRequest);
+}

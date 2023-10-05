6fd9898ca24df6faaa12abd6d3293567ab3d9e9a
Iggy988 <igor.rodic1988@gmail.com>
2023-10-05 22:15:21 +0200

    Implement Get Method in VillaNumberController

--- MagicVilla_WebProject/Models/Dto/VillaNumberDTO.cs
+++ MagicVilla_WebProject/Models/Dto/VillaNumberDTO.cs
@@ -6,11 +6,9 @@ public class VillaNumberDTO
 {
     [Required]
     public int VillaNo { get; set; }
-    //[Required]
-    //public int VillaID { get; set; }
     [Required]
     public int VillaID { get; set; }
     public string SpecialDetails { get; set; }
-   // public VillaDTO Villa { get; set; }
+    public VillaDTO Villa { get; set; }
 }
 
